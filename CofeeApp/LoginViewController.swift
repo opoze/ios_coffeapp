5fdaaf58a49271cfe5dfae391cd400e93077964d
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2019-06-05 08:59:02 -0300

    first commit

---
+++ CofeeApp/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  CofeeApp
+//
+//  Created by Luis Alberto Zagonel Pozenato on 04/06/2019.
+//  Copyright © 2019 LuisPozenato. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
