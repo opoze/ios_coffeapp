a3e4c368b4c7f1efca50d62a2e0d58b351837c44
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2019-06-05 08:59:40 -0300

    Initial Commit

--- CofeeApp/Extensions.swift
+++ CofeeApp/Extensions.swift
@@ -37,3 +37,16 @@ extension DesignableButton{
         }
     }
 }
+
+extension UITextField {
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
